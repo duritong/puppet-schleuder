b82d91494194473e04c47c0d6e185bb13d003271
mh <mh@immerda.ch>
2009-05-14 19:46:31 +0200

    init of schleuder module

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+#
+# schleuder module
+#
+# Copyright 2009, admin(at)immerda.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+class schleuder {}
