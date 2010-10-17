cf9307a45f1ac350deb646bd0bed0fb87e910b26
mh <mh@immerda.ch>
2010-10-17 16:32:12 +0200

    checking on the right parameter

--- manifests/list.pp
+++ manifests/list.pp
@@ -93,7 +93,7 @@ define schleuder::list(
   if $ensure == present {
     Exec["manage_schleuder_list_${name}"]{
       command => "${schleuder_install_dir}/contrib/newlist.rb ${name} -email ${email} -realname \"${real_realname}\" -adminaddress ${adminaddress} -initmember ${real_initmember} -initmemberkey /var/schleuderlists/initmemberkeys/${name}_${real_initmemberkey}.pub -nointeractive -mailuser ${run_as}",
-      require => $manage_alias ? {
+      require => $manage_run_as ? {
         true => [ User::Managed[$real_run_as], File["/var/schleuderlists/initmemberkeys/${name}_${real_initmemberkey}.pub"] ],
         default => File["/var/schleuderlists/initmemberkeys/${name}_${real_initmemberkey}.pub"]
       },
