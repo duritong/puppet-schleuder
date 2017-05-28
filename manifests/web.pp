a974576b21fc8ac16d545d25b416c73014b11d3c
mh <mh@immerda.ch>
2017-05-28 02:22:11 +0200

    some fixes, add shorewall support to web & monkeypatch for config

--- manifests/web.pp
+++ manifests/web.pp
@@ -8,11 +8,28 @@ class schleuder::web(
   $mailer_from         = 'noreply@example.org',
   $database_config     = {},
   $ruby_scl            = 'ruby23',
+  $use_shorewall       = false,
 ){
   require "::scl::${ruby_scl}"
   package{'schleuder-web':
     ensure => present,
   } -> file{
+    '/var/www/schleuder-web/config/initializers/01_erb_config.rb'
+      content => "# https://0xacab.org/schleuder/schleuder-web/issues/62
+module Squire
+  module Parser
+    module YAML
+      def self.parse(path)
+        ::YAML::load(ERB.new(File.read(path)).result)
+      end
+    end
+  end
+end
+",
+      replace => false,
+      owner   => root,
+      group   => 'schleuder-web',
+      mode    => '0640';
     '/var/www/schleuder-web/config/database.yml':
       content => template('schleuder/web/database.yml.erb'),
       owner   => root,
@@ -25,9 +42,12 @@ class schleuder::web(
       mode    => '0640';
   } ~> exec{'setup-schleuder-web':
     command     => "scl enable rh-${ruby_scl} 'bundle exec rake db:setup RAILS_ENV=production'",
-    pwd         => '/var/www/schleuder-web',
+    cwd         => '/var/www/schleuder-web',
     refreshonly => true,
     user        => 'schleuder-web',
     group       => 'schleuder-web',
+  }
+  if $use_shorewall and $api_host != 'localhost' {
+    include schleuder::web::shorewall
   }
 }
---
+++ manifests/web/shorewall.pp
@@ -0,0 +1,20 @@
+# schleuder api daemon
+class schleuder::shorewall {
+  $port = $schleuder::web::api_port
+  shorewall::rule { "me-net-${port}-tcp":
+    source          => '$FW',
+    destination     => 'net',
+    proto           => 'tcp',
+    destinationport => $port,
+    order           => 240,
+    action          => 'ACCEPT';
+  }
+  # must be ready before setting up
+  Service['shorewall'] -> Exec['setup-schleuder-web']
+
+  if $schleuder::web::database_config['adapter'] == 'postgresql' {
+    include shorewall::rules::out::postgres
+  } elsif $schleuder::web::database_config['adapter'] == 'mysql' {
+    include shorewall::rules::out::mysql
+  }
+}
