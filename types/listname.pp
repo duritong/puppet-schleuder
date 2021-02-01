0b2ec8bf249a1a84a4af9e9be87b01fe256eaf88
mh <mh@immerda.ch>
2021-02-01 14:45:57 +0100

    also allow points

--- types/listname.pp
+++ types/listname.pp
@@ -1,1 +1,1 @@
-type Schleuder::Listname = Pattern[/^[a-z0-9_\-]+@(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/]
+type Schleuder::Listname = Pattern[/^[a-z0-9_\-\.]+@(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/]
