e219fad3d69beee9c7e19fd7d23316f479611a70
mh <mh@immerda.ch>
2023-10-20 22:47:31 +0200

    File.exists? is gone in newer ruby versions

--- lib/facter/schleuder_tls_fingerprint.rb
+++ lib/facter/schleuder_tls_fingerprint.rb
@@ -1,9 +1,9 @@
 Facter.add('schleuder_tls_fingerprint') do
   setcode do
     # check whether schleuder could actually find a cert
-    if File.exists?('/etc/schleuder/schleuder.yml') &&
+    if File.exist?('/etc/schleuder/schleuder.yml') &&
         (config = YAML.load_file('/etc/schleuder/schleuder.yml')) &&
-        File.exists?(config['api']['tls_cert_file']) &&
+        File.exist?(config['api']['tls_cert_file']) &&
         (fp = Facter::Util::Resolution.exec('schleuder cert fingerprint'))
       fp.split(': ').last
     else
